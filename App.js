d3bd5235eca608af7975625c80e985753bd42c8c
anveshishori <74931675+anveshishori@users.noreply.github.com>
2021-06-27 01:02:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen'
+
+
+
+export default class App extends React.Component {
+  
+  render(){
+  
+  return (
+   
+    <HomeScreen/>
+
+  );
+  }
+}
+
---
+++ screens/HomeScreen.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
+
+
+export default class HomeScreen extends React.Component{
+render(){
+
+    return(
+ 
+        <View style = {{marginTop : 200}}>
+
+        <Text> THIS IS THE HOME SCREEN FOR THE APP!! </Text>
+
+        </View>
+
+
+
+
+
+
+    )
+
+
+
+
+
+}
+
+
+
+
+}
